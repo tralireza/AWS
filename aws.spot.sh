4650eaffd8a3a28254efabe48afaf9bedf5ac0a2
Alireza Torabi <26859725+tralireza@users.noreply.github.com>
2021-10-22 06:50:13 +0100

    .

---
+++ aws.spot.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+aws --profile at --region us-east-1 --output json  ec2 request-spot-instances \
+    --spot-price "0.015" --instance-count 1 --type "one-time" --launch-specification file://launch.spec.json
