4c9031012f31ca01c4c92068f3979d73fba822c1
Alireza <26859725+tralireza@users.noreply.github.com>
2023-04-19 19:51:55 +0100

    $ spot: t3.micro

--- aws.spot.sh
+++ aws.spot.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 aws --profile at --region us-east-1 --output yaml ec2 request-spot-instances \
-    --spot-price "0.01" --instance-count 1 --type "one-time" --launch-specification file://launch.spec.json
+    --spot-price "0.0075" --instance-count 1 --type "one-time" --launch-specification file://launch.spec.json
