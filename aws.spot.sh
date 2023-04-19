47d36c75b0d20c21a64bc81b94a6a62e7f4dd87b
Alireza <26859725+tralireza@users.noreply.github.com>
2023-04-19 09:57:27 +0100

    output: yaml

--- aws.spot.sh
+++ aws.spot.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-aws --profile at --region us-east-1 --output json ec2 request-spot-instances \
+aws --profile at --region us-east-1 --output yaml ec2 request-spot-instances \
     --spot-price "0.01" --instance-count 1 --type "one-time" --launch-specification file://launch.spec.json
